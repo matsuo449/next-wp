9ecde7bd5fae7a1b2df55eb1690b3a1cbcf7617f
naoki matsuo <naoki_matsuo@012grp.co.jp>
2020-07-17 19:10:19 +0900

    no message

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import '../styles/style.css'
 import Head from 'next/head'
 
 // compornents
-import Header from '../components/header'
-import Footer from '../components/footer'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }) {
   return (
